4555e69f40ad20af5170b81bd46ba278ff0f928b
Omer Hanan <omer.hanan@gmail.com>
2023-05-03 10:35:56 +0300

    Update 1.1

--- R_script.R
+++ R_script.R
@@ -13,6 +13,10 @@ sapply(data, function(x) sum(is.na(x)))
 # Count empty strings values:
 sapply(data, function(x) sum(nchar(as.character(data$x))))
 
-
-
-  
+# Update categorical values as factors with them unique values:
+col_names <- c("Gender", "Married", "Dependents", "Education", "Self_Employed", 
+               "Credit_History", "Property_Area", "Loan_Status")
+for (col in col_names) {
+  col_levels <- unique(data[[col]])
+  data[[col]] <- factor(data[[col]], levels = col_levels)
+}
